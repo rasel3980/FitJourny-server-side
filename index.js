dd16ba25957b86d7a02beeca78db850737c6816a
rasel3980 <rkrasel3980@gmail.com>
2025-01-20 09:37:14 +0600

    Get trainer details

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json())
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.n4ll4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -34,6 +34,13 @@ async function run() {
     app.get('/trainer', async(req, res) =>{
       const result = await trainersCollection.find().toArray();
       res.send(result);
+  })
+// trainer details
+  app.get('/trainer-details/:id', async(req,res)=>{
+    const id = req.params.id;
+    const query = {_id: new ObjectId(id)};
+    const result = await trainersCollection.findOne(query);
+    res.send(result);
   })
 
     // jwt related api
